bd5cad88d5067e404b037e0a6f1ddddad2f9256e
chhabiraniojha <chhabiraniojha@gmail.com>
2023-11-24 23:05:15 +0530

    test again 404

--- Client/expenceTracker/vite.config.js
+++ Client/expenceTracker/vite.config.js
@@ -5,16 +5,5 @@ import pluginRewriteAll from 'vite-plugin-rewrite-all';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),pluginRewriteAll()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-  alias: {
-    '/@': path.resolve(__dirname, 'src'),
-  }
+  base: '/', // Specify the base URL here
 })
